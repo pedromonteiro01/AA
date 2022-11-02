18f1a08076c7699ac0913cced066a4825e0ab28b
Pedro Monteiro <64958453+pedromonteiro01@users.noreply.github.com>
2022-11-02 15:02:30 +0000

    Update min_edge_cover.py

--- project1/min_edge_cover.py
+++ project1/min_edge_cover.py
@@ -1,12 +1,13 @@
 # @Author: Pedro Monteiro
 # @Email:  pmapm@ua.pt
 
-
 import json, itertools, time
 
 from matplotlib.pyplot import get
 import os
-from utils import get_adjacency_list
+
+from utils import get_adjacency_list, show_incidence_matrix
+
 
 def get_graph(n,p):
     file = 'graph'+str(n)+'-'+str(p)+'.txt'
@@ -14,75 +15,45 @@ def get_graph(n,p):
         with open(file) as f:
             return json.loads(f.readline()), json.loads(f.readline())
 
-def check_isolated_graph(v,e):
-    graph_vertices = {vertex[0] for vertex in v}
-    vertices_in_edges = {}
-    for element in e:
-        vertices_in_edges.update({element[0], element[1]})
-    
-    if len(graph_vertices) == len(vertices_in_edges):
-        return True
+def find_solution(v,e):
+    for i in range(1, len(v)+1):
+        for data in itertools.combinations(e, i): # get all subsets of edges
+            temp_set = set()
+            #print("data: ",list(data))
+            for item in data:
+                temp_set.update({item[0], item[1]}) # add {'x1', 'x2} to set
+                
+                if len(v) == len(temp_set): # only covering edges here!
+                    return i
 
-    return False
-
-def get_vertex_with_less_edges(adj_list, lst):
-    vertex_dic = {} # {vertex: number_of_neighbours}
-    for v in lst:
-        vertex_dic[v] = len(adj_list[v])
-
-    ordered_vertex_dic = dict(sorted(vertex_dic.items(), key=lambda item: item[1]))
-
-    if list(ordered_vertex_dic.keys()):
-        return list(ordered_vertex_dic.keys())[0]
-    
     return None
 
-
-def find_solution(v,e):
-    associated_vertices = set()
-    if not check_isolated_graph:
-        return None
-    
-    adj_list = get_adjacency_list(e)
-
-    # get ordered vertexes with less edges
-    sort_vertex_by_num_edges = dict(sorted(adj_list.items(), key=lambda item: len(item[1])))
-
-    min_edge = 0
-    for vertex in sort_vertex_by_num_edges.keys():
-        if vertex not in associated_vertices:
-            min_edge += 1
-            min_vertex = get_vertex_with_less_edges(adj_list, sort_vertex_by_num_edges[vertex])
-
-            while min_vertex in associated_vertices:
-                adj_list2 = adj_list.copy()
-                if min_vertex in adj_list2[vertex]:
-                    adj_list2[vertex].remove(min_vertex)
-                else:
-                    break
-                min_vertex = get_vertex_with_less_edges(adj_list2, sort_vertex_by_num_edges[vertex]) 
-
-            associated_vertices.update({vertex,min_vertex})
-
-        if len(associated_vertices) == len(v):
-            break
-    
-    return min_edge
+def find_solution_with_inc_matrix(v,e):
+    v_list = [vertex[0] for vertex in v] # set with all vertexes
+    for i in range(1, len(v_list)+1):
+        for data in itertools.combinations(e, i): # get all subsets of edges
+            adj = get_adjacency_list(list(data))
+            if [] not in adj.items() and len(adj) == len(v_list): # when all vertices from the graph have at least 1 edge
+                return i
 
 percentages = [12.5, 25, 50, 75]
-with open('greedy_results2.txt', 'w') as f:
-    f.write(f"n \t\tpercentage \t\tedges \t\tmin_edge \t\ttime \t\titerations\n")
-    for i in range(4, 52):
+with open('brute_force_results.txt', 'w') as result_file:
+    #result_file.write(f"n \t\tpercentage \t\tedges \t\tmin_edge \t\ttime\n")
+    for i in range(2,25):
         for p in percentages:
             file = 'graph'+str(i)+'-'+str(p)+'.txt'
             if os.path.exists(file):
                 with open(file) as f:
                     v,e = json.loads(f.readline()), json.loads(f.readline())
                     v,e = get_graph(i,p)
+                    #start = time.time()
+                    #min_edge = find_solution(v,e)
+                    #end = (time.time() - start)
+                    #inc_matrix = show_incidence_matrix(v,e)
                     start = time.time()
-                    min_edge = find_solution(v,e)
+                    min_edge = find_solution_with_inc_matrix(v,e)
                     end = (time.time() - start)
-                    #f.write("%s %10s %10s %10s %10s %10s\n" % (len(v), p, len(e), min_edge, end, iterations))
+                    #result_file.write("%s %10s %10s %10s %10s\n" % (len(v), p, len(e), min_edge, end))
                     print("num_v: ", len(v), "num_edges: ", len(e), "percentage: ", p)
-                    print(min_edge)
+                    print("min_edge: ",min_edge)
                     print(end)

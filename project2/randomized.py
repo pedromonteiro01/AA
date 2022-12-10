277e93037e5454ab0cd09feec2be240c34a08f81
Pedro Monteiro <pmapm@ua.pt>
2022-12-10 11:51:55 +0000

    create monte carlo approach

--- project2/randomized.py
+++ project2/randomized.py
@@ -1,4 +1,4 @@
-import os, json, random, time
+import os, json, random, time, itertools
 
 def get_graph(n,p):
     file = 'graph'+str(n)+'-'+str(p)+'.txt'
@@ -7,41 +7,62 @@ def get_graph(n,p):
             return json.loads(f.readline()), json.loads(f.readline())
 
 def get_new_graph():
-    file = 'SWlargeG.txt'
+    file = 'lastfm_asia_edges.csv'
     edges = []
     vertices = set()
     if os.path.exists(file):
         with open(file) as f:
             for line in f:
-                line = line.strip().split(" ")
+                line = line.strip().split(",")
                 edges.append([line[0], line[1]])
                 vertices.update({line[0], line[1]})
     
     return list(vertices), edges
 
+def get_edges_number(e):
+    return len(e)
 
-def randomized_search():
-    v,e = get_graph(14,50)
-    #v,e = get_new_graph()
+
+def randomized_search(v,e):
+    operations = 0 # initialize operations counter variable
     edges_copy = e[:] # copy edges list
     unique_vertices = set()
     min_edge = 0
-    while len(unique_vertices) != len(v): # while all vertices are not chosen
-        selected_edge = random.choice(e) # pick a random edge
+    operations += 4
+    while len(unique_vertices) != len(v): # while all vertices are not chosen - O(v) vezes
+        selected_edge = random.choice(edges_copy) # pick a random edge
+        operations += 1
         if selected_edge in edges_copy: # check if was already picked
-            edges_copy.remove(selected_edge) 
+            edges_copy.remove(selected_edge) # O(e) vezes
             unique_vertices.update({selected_edge[0], selected_edge[1]}) # add to set with vertices already chosen
             min_edge+=1
+            operations += 2
 
-    return min_edge, len(e)
+    return min_edge, operations
 
-min_prev=9999999999999
-num_simulations = 100000
-start = time.time()
-for i in range(num_simulations):
-    min_edge, edge = randomized_search()
-    if min_edge < min_prev:
-        min_prev = min_edge
-end = time.time()
-print("time: ", end - start)
-print("min edge: ", min_prev)
+percentages = [12.5, 25, 50, 75]
+with open('randomized_results.txt', 'w') as result_file:
+    #result_file.write(f"n \t\t\tpercentage \t\t\tedges \t\t\t\tmin_edge \t\t\t\toperations \t\ttime \n")
+    for n in range(3,21):
+        for p in percentages:
+            file = 'graph'+str(n)+'-'+str(p)+'.txt'
+            if os.path.exists(file):
+                with open(file) as f:
+                    v,e = json.loads(f.readline()), json.loads(f.readline())
+
+                    min_prev=9999999999999 # set variable high to be updated
+                    num_edges = get_edges_number(e) # get edges number -> len(e)
+                    num_simulations = max([20000, round(0.2 * 2**(num_edges)-1)]) # calculate number of simulations, using 60%
+                    #print("Iterations: ", num_simulations)
+                    start = time.time()
+                    timeout = time.time() + 60*2   # 2 minutes from now
+                    for i in range(num_simulations):
+                        min_edge, operations = randomized_search(v,e) # perform randomized search
+                        if min_edge < min_prev: # update min edge value when is less than the previous one
+                            min_prev = min_edge
+                        if time.time() > timeout: # prevent algorithm from taking to much time to run
+                            print("exceeded time")
+                            break
+                    end = time.time()
+                    #result_file.write(f"{len(v)} \t\t\t{p} \t\t\t\t\t{len(e)} \t\t\t\t\t{min_prev} \t\t\t\t\t\t{operations} \t\t\t\t{end - start}\n")
+                    print("Vertices: ", len(v),"%: ", p,"time: ", end - start, "min edge: ", min_prev, "operations: ", operations)

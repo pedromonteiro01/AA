73fa982d94a40031638166bb453e7a8782cda1f9
Pedro Monteiro <pmapm@ua.pt>
2022-12-28 15:01:41 +0000

    add some comments

--- project3/exact_counters.py
+++ project3/exact_counters.py
@@ -1,22 +1,24 @@
 from utils import get_upper_letters
 
-# get letters
+# get letters from file
 letters = get_upper_letters("pg69638.txt")
-print(letters)
 
-# create a dictionary to store the letter counts
+# dictionary to store letter counts
 letter_counts = {}
 
+# iterate all letters
 for letter in letters:
-    if letter in letter_counts:
+    if letter in letter_counts: # if letters is already in dict, just count +1
         letter_counts[letter] += 1
-    else:
+    else: # else initialize a counter to a new letter
         letter_counts[letter] = 1
 
 k_values = [3, 5, 10]
 for k in k_values:
     with open(f"exact_counter_results/exact_counter-{k}.txt", "w") as f:
         f.write(f"Most Frequent Letters with k = {k}\n\n")
+
+        # sort dict by values (higher values first) and get k first letters
         sorted_letters = sorted(letter_counts.items(), key=lambda x: x[1], reverse=True)[:k]
 
         for item in sorted_letters:
